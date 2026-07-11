13d96557cb7c85fbb53ad5cc3c85e036b1a5efad
liya2133 <liyasebastian2108@gmail.com>
2026-07-11 12:58:04 +0530

    search completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import AddReg from './components/AddReg'
 import SearchReg from './components/SearchReg'
+import DeleteReg from './components/DeleteReg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
      <AddReg />
      <SearchReg />
+     <DeleteReg />
     </>
   )
 }
