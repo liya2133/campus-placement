8a3f37f09d13e87a946e1553519b54a77840100f
liya2133 <liyasebastian2108@gmail.com>
2026-07-11 12:45:38 +0530

    search completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddReg from './components/AddReg'
+import SearchReg from './components/SearchReg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
      <AddReg />
+     <SearchReg />
     </>
   )
 }
