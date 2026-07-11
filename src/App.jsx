0f75c7108052de810292c7ba91c31be8caf845fb
liya2133 <liyasebastian2108@gmail.com>
2026-07-11 13:07:52 +0530

    view completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import AddReg from './components/AddReg'
 import SearchReg from './components/SearchReg'
 import DeleteReg from './components/DeleteReg'
+import ViewReg from './components/ViewReg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
      <AddReg />
      <SearchReg />
      <DeleteReg />
+     <ViewReg />
     </>
   )
 }
